d2f8ad8a0b66655779bac48ed75c6c56c3609b02
rethinkux <rethinkux@gmail.com>
2018-12-30 11:01:49 +0530

    Make GET request with Axios in react

---
+++ src/components/FullPost/FullPost.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import './FullPost.css';
+import Axios from 'axios';
+
+class FullPost extends Component {
+    state = {
+        loadedPost: null,
+    }
+
+    componentDidUpdate() {
+        if(this.props.id){
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                Axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id).then(
+                    response => {
+                        // console.log(response);
+                        this.setState({ loadedPost: response.data })
+                    }
+                );
+            }
+        }
+    }
+
+    render () {
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        if(this.props.id){
+            <p style={{ textAlign: 'center' }}>Loading...</p>;
+        }
+        // null is treated as false. so when id will be null 
+        // then this.props.id will return false.
+        if(this.state.loadedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+
+            );
+        }
+        return post;
+    }
+}
+
+export default FullPost;
